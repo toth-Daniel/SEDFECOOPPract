e116e902451dd792fbe7700a3a3696a0d361d465
Tóth Dániel <toth.daniel420@gmail.com>
2025-04-10 21:05:28 +0200

    Create MintaPr1.java

---
+++ SEDFEC_0410/MintaPr1.java
@@ -0,0 +1,11 @@
+public class MintaPr1
+{
+	public static void main(String[] args)
+	{
+		MintaOsztaly obj = new MintaOsztaly(15, 25);
+		obj.Output();
+
+//		MintaOsztaly.setNumber(15, 25);
+//		MintaOsztaly.staticOutput();
+	}
+}
