cfb93daa2683cec96e742cb26ad36311c8e2aa6f
Tóth Dániel <toth.daniel420@gmail.com>
2025-04-21 15:17:35 +0200

    Create SEDFECProg1.java

---
+++ SEDFEC_0417/SEDFECProg1.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class SEDFECProg1 extends JFrame
+{
+
+	SEDFECProg1()
+	{
+		setSize(400, 300);
+		setVisible(true);
+	}
+
+	public static void main(String[] args)
+	{
+		new SEDFECProg1();
+	}
+}
